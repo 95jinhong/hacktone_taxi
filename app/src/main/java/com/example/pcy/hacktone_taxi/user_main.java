4ec870986e23667d3faf2ba89ba45da845199be2
park chanyoung <pcx474@gmail.com>
2018-11-16 15:52:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pcy/hacktone_taxi/user_main.java
@@ -0,0 +1,13 @@
+package com.example.pcy.hacktone_taxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
+}
